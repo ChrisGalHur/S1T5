975b79af8488441f8a99d79603d2532d6b05a704
Christian <christian.galvez.hurtado@hotmail.com>
2022-10-24 13:27:58 +0200

    Casi acabado

--- src/S1T5N1Ex4/Main4.java
+++ src/S1T5N1Ex4/Main4.java
@@ -1,7 +1,9 @@
-package S1T5N1Ex3;
+package S1T5N1Ex4;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
@@ -10,23 +12,24 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main3 {
+public class Main4 {
 
 	private static int count = 0;
 	
 	public static void main(String[] args) {
-		/*- Exercici 3
-		Modifica l’exercici anterior. Ara, en lloc de mostrar el resultat per la pantalla, guarda el resultat en un fitxer TXT.*/
-		
-		//Le he dado la vuelta a todo para que quede más limpio
+		/*- Exercici 4
+		Afegeix la funcionalitat de llegir qualsevol fitxer TXT i mostra el seu contingut per consola.*/
 
 		File dirMain;
 		String m = args[0];
 		String arbreMain = "";
 		
 		dirMain = llegirFitxer(m);
-		arbreMain += ferArbreArchius(dirMain, arbreMain, 0);
-//		guardarRegistre(arbreMain);
+		if(dirMain.getName().contains(".txt")) {
+			llegirTxt(dirMain);
+		}else {
+			arbreMain += ferArbreArchius(dirMain, arbreMain, 0);
+		}
 		
 	}
 
@@ -34,10 +37,10 @@ public class Main3 {
 		File dir = new File(a);
 		File[] ficheros = dir.listFiles();
 			
-		if (ficheros == null) {
+		if (ficheros == null && dir.getName().contains(".txt") == false) {
 			  System.out.println("No hay ficheros en el directorio especificado");
 		}
-		return dir;
+	return dir;
 	}
 	
 	public static String ferArbreArchius(File fileATreballar, String arbre, Integer iLevel) {
@@ -80,5 +83,26 @@ public class Main3 {
         }
 		System.out.println(resposta);
 	}
+	
+	public static void llegirTxt(File llegir) {
+		File arxiuLlegir = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+	      
+		try {
+			fr = new FileReader (llegir);
+			br = new BufferedReader(fr);
+			
+			String linea;
+			while((linea = br.readLine()) != null)
+	            System.out.println(linea);
+			
+		fr.close();
+		}catch(IOException e){
+			System.out.println("No puedo leer el archivo");
+		}	
+		
+	}
+	
 	
 }
